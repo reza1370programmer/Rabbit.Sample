de66507eb6c85491c69bab635fe467eb11ce926f
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2025-04-22 11:20:00 +0330

    gg

--- Rabbit.Sample.Consumer/Program.cs
+++ Rabbit.Sample.Consumer/Program.cs
@@ -3,25 +3,41 @@ using RabbitMQ.Client.Events;
 using System.Text;
 
 
-var ConnectionFactory = new ConnectionFactory()
-{
-    HostName = "localhost"
-};
-var Connection = await ConnectionFactory.CreateConnectionAsync();
-var Channel = await Connection.CreateChannelAsync();
+var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
 
-await Channel.QueueDeclareAsync("MyQueue", false, false, false, null);
-var ConsumerEvent = new AsyncEventingBasicConsumer(Channel);
-ConsumerEvent.ReceivedAsync += (s, e) =>
-{
-    var body = e.Body.ToArray();
-    string message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($"message: {message}");
-    return Task.CompletedTask;
-};
-await Channel.BasicConsumeAsync("MyQueue", true, ConsumerEvent);
+string exchangeName = "reza_exchange_direct";
+string routingKey = "rezainfo";
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
 
-Console.WriteLine("consumer finished");
+// Use a named queue instead of temporary one
+string queueName = "reza_queue";
+await channel.QueueDeclareAsync(
+    queue: queueName,
+    durable: true,
+    exclusive: false,
+    autoDelete: false);
+
+await channel.QueueBindAsync(queueName, exchangeName, routingKey);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    // Manual acknowledgment if autoAck is false
+    // await channel.BasicAckAsync(ea.DeliveryTag, false);
+};
+
+await channel.BasicConsumeAsync(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" [*] Waiting for messages. Press any key to exit");
 Console.ReadKey();
 
 //basic usage of rabbit includes declaring queue and using default exchange and specifying queue for exchange and consumer
--- Rabbit.Sample.Producer/Program.cs
+++ Rabbit.Sample.Producer/Program.cs
@@ -1,23 +1,23 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-var ConnectionFactory = new ConnectionFactory()
-{
-    HostName = "localhost"
-};
-var Connection = await ConnectionFactory.CreateConnectionAsync();
-var Channel = await Connection.CreateChannelAsync();
+ConnectionFactory connectionFactory = new() { HostName = "localhost" };
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
 
+string exchangeName = "reza_exchange_direct";
+string routingKey = "rezainfo";
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
 
 string message = "hi reza from producer";
 var body = Encoding.UTF8.GetBytes(message);
 
-string ExchangeName = "reza_exchange_direct";
-string RoutingKey = "reza_info";
-await Channel.ExchangeDeclareAsync(ExchangeName, ExchangeType.Direct);
+await channel.BasicPublishAsync(
+    exchange: exchangeName,
+    routingKey: routingKey,
+    body: body);
 
-await Channel.BasicPublishAsync(ExchangeName, RoutingKey, body);
-Console.WriteLine("message sent");
+Console.WriteLine($" [x] Sent '{message}'");
 Console.ReadKey();
 
 //basic usage of rabbit includes declaring queue and using default exchange and specifying queue for exchange and consumer
