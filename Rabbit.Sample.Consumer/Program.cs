5104ec4bf0368896a1220b76ae13734d8606bce2
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2025-04-22 09:40:58 +0330

    Add project files.

---
+++ Rabbit.Sample.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+var ConnectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+var Connection = await ConnectionFactory.CreateConnectionAsync();
+var Channel = await Connection.CreateChannelAsync();
+
+await Channel.QueueDeclareAsync("MyQueue", false, false, false, null);
+var ConsumerEvent = new AsyncEventingBasicConsumer(Channel);
+ConsumerEvent.ReceivedAsync += (s, e) =>
+{
+    var body = e.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"message: {message}");
+    return Task.CompletedTask;
+};
+await Channel.BasicConsumeAsync("MyQueue", true, ConsumerEvent);
+
+Console.WriteLine("consumer finished");
+Console.ReadKey();
---
+++ Rabbit.Sample.Producer/Program.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var ConnectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+var Connection = await ConnectionFactory.CreateConnectionAsync();
+var Channel = await Connection.CreateChannelAsync();
+
+await Channel.QueueDeclareAsync("MyQueue", false, false, false, null);
+string message = "hi reza from producer";
+var body = Encoding.UTF8.GetBytes(message);
+await Channel.BasicPublishAsync("", "MyQueue", body);
+Console.WriteLine("message sent");
+Console.ReadKey();
